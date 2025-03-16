2447594fd99a4f3049e772140f1ca84c38140569
Khojiakbar <khojiakbar_kodirov@mail.ru>
2025-03-16 15:18:09 +0900

    feat: MUI standard integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import '../css/app.css';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div>HOME</div>
+    <Container maxWidth="sm"> 
+      <Stack flexDirection={"column"}>
+        <Box sx={{my: 4}}>
+          <Typography variant="h4" component={"h4"}>
+            Create Something
+          </Typography>
+        </Box>
+        <Button variant="contained">Contained</Button>
+      </Stack>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -25,6 +25,9 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import  CssBaseline  from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme';
 import './css/index.css';
 
 const container = document.getElementById('root')!;
@@ -33,7 +36,10 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
